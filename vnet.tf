9e70e7c4c7c0e7e49b8201405e57d96bd74695a0
Deepthi Surampalli <deepusurampalli@gmail.com>
2025-10-31 12:48:31 +0530

    hello

--- vnet.tf
+++ vnet.tf
@@ -4,8 +4,10 @@ resource "azurerm_virtual_network" "vnet" {
     resource_group_name = azurerm_resource_group.rg.name
     location = azurerm_resource_group.rg.location
 
-    
+   tags = {
+    name = "vcube"
 }
+} 
 
 resource azurerm_subnet "subnet" {
     address_prefixes = ["172.16.1.0/24"]
