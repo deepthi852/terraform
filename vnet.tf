87eb717a6bd8d98f717c756f44c6f4b1916f1ecc
Deepthi Surampalli <deepusurampalli@gmail.com>
2025-10-31 12:56:43 +0530

    added new tags

--- vnet.tf
+++ vnet.tf
@@ -4,22 +4,12 @@ resource "azurerm_virtual_network" "vnet" {
     resource_group_name = azurerm_resource_group.rg.name
     location = azurerm_resource_group.rg.location
 
-<<<<<<< HEAD
+
    tags = {
     name = "vcube"
 }
 } 
-=======
-<<<<<<< HEAD
-   tags = {
-    name = "krishna"
-=======
-    
->>>>>>> 905809e6eabc0dd1863f49ac4060d312076fd14f
-}
-}
-
->>>>>>> 10af2cadb579f4656bd4c0d780f8ef121a7c7c3e
+   
 
 resource azurerm_subnet "subnet" {
     address_prefixes = ["172.16.1.0/24"]
